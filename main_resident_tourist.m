62aa07c14d5f4cc252d26796c04025c92d244a33
ponyo7 <yuanwang0714@gmail.com>
2018-04-30 21:49:13 -0700

    add av adoption rate to simulation

--- main_resident_tourist.m
+++ main_resident_tourist.m
@@ -3,9 +3,11 @@
 % attribute
 % 2. compute revenue for reach person (each row)
 
+av_adoption_rate = 0.01;%0:0.05:1;
+
 %----------------------step1 generate simulation table---------------------
-%1*9, 1: IDs,2: trip purpose 3: zip codes 4: travel modes 5: activity 6: parking mode, 7: parking time 8: distance 9: revenue
-simulation_all = generate_simulation_table();
+%1*10, 1: IDs,2: trip purpose 3: zip codes 4: travel modes 5: activity 6: parking mode, 7: parking time 8: distance 9: AV 10: revenue
+simulation_all = generate_simulation_table(av_adoption_rate);
 num_rows = size(simulation_all);
 
 %----------------------step2 compute revenue for each row------------------
@@ -24,6 +26,6 @@ for i = 1:numrows
     if(row(4)==4 || row(4)==5) % comfortable or economic
         revenue = revenue_comfortable_economic(row);
     end
-    simulation_all(i,9) = revenue;
+    simulation_all(i,10) = revenue;
 end
 
