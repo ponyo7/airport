8e879b1500f08b6f61df703de9e48b74b5a85bdc
Yuan Wang <yuanwang0714@gmail.com>
2018-05-08 23:35:57 -0400

    change input file names

--- generate_simulation_table.m
+++ generate_simulation_table.m
@@ -2,18 +2,14 @@ function simulation_all = generate_simulation_table(av_adoption_rates)
 %zipcodes_enplanement_resident,zipcodes_enplanement_tourist,zipcode_distance,
 %percent_travel_mode_resident,percent_travel_mode_tourist,percent_activity_private_car,percent_activity_rentalcar,percent_parking_mode,
 %st_hourly_mu,st_hourly_sigma,st_daily_mu,st_daily_sigma,lt_daily_mu,lt_daily_sigma,eco_parking_mu,eco_parking_sigma)
-%This function create a n*9 simulation table. Each row represents a person.
-
-% Please refer to the comments for what each col represent
-%
-%TODO: pull all constants and input here
+%This function create a n*12 simulation table. Each row represents a person.
 
 %--------------------input prepare-----------------------------------------
 %input enplamenets zipcode file by resident and tourist, keep the files have same zipcode list
-zipcodes_enplanement_resident = dlmread('zipcode_enplanement_resident.txt');%nx2 zipcode, enplanements
-zipcodes_enplanement_tourist = dlmread('zipcode_enplanement_tourist.txt');%nx2 zipcode, enplanements
+zipcodes_enplanement_resident = dlmread('SFO_zipcode_enplanement_resident.txt');%nx2 zipcode, enplanements
+zipcodes_enplanement_tourist = dlmread('SFO_zipcode_enplanement_tourist.txt');%nx2 zipcode, enplanements
 zipcodes_resident = zipcodes_enplanement_resident(:,1);
-zipcodes_tourist = zipcodes_enplanement_tourist(:,2);
+zipcodes_tourist = zipcodes_enplanement_tourist(:,1);
 enplanements_resident = zipcodes_enplanement_resident(:,2); %numbers must be positive or 0
 enplanements_tourist = zipcodes_enplanement_tourist(:,2); %enplanemnts must have same # of rows
 assert(size(enplanements_resident, 1)==size(enplanements_tourist,1))
@@ -25,23 +21,22 @@ end
 enplanements_all = enplanements_resident + enplanements_tourist;%two zipcode enplanement files have the same zipcode list
 %input distance file by zipcode
 num_zipcode = size(enplanements_all,1); 
-%zipcode_distance = dlmread('Distance.txt'); %1. zipcode, 2. google-map
-%distance---------------------------------------------------------------------FIXME prepare distance.txt and remove the next line---------------------------------------------------------------------
-zipcode_distance = zeros(num_zipcode, 2) + 10000;
+zipcode_distance = dlmread('SFO_distance.txt'); %1. zipcode, 2. google-map distance in meters
 assert(num_zipcode == size(zipcode_distance,1)); % each row of enplanement and distance should corresponde to the same zipcode
 
 num_all_IDs = sum(enplanements_all, 1);
 num_residents = sum(enplanements_resident, 1);
 num_tourist = sum(enplanements_tourist, 1);
 
-%All fake numbers for now, FIXME: get real numbers
-%!!!!!!!!DONT change the order of travel mode!!! have to be consistent with
-%slides!!!!!!!!!!!!!!!!!!!
-percent_travel_mode_resident = [0.25, 0.25, 0, 0.25, 0.25]; %FIXME: fill them in real numbers. 1. private car, 2. tnc and taxis, 3. rental car 4. comfortable public transit, 5. economic public transit 
-percent_travel_mode_tourist = [0, 0.25, 0.25, 0.25, 0.25]; %FIXME: fill them in real numbers. 1. private car, 2. tnc and taxis, 3. rental car 4. comfortable public transit, 5. economic public transit
-percent_activity_private_car = [1/3, 1/3, 1/3, 0, 0]; %park_at_airport, park_off_airport, curbside, 
-percent_activity_rental_car = [0, 0, 0,0.5 0.5]; % rentalcar_on_airport, rentalcar_off_airport
-percent_parking_mode = [0.5 0.2 0.3 0]; %short term hourly, short term daily, long term, economic parking 
+%input parameters related to travel modes
+percent_travel_mode_resident = [0.49, 0.33, 0, 0.08, 0.10]; %SFO data from survey, 1. private car, 2. tnc and taxis, 3. rental car 4. comfortable public transit, 5. economic public transit 
+percent_travel_mode_tourist = [0.27, 0.34, 0.19, 0.11, 0.09]; %%SFO data from survey, 1. private car, 2. tnc and taxis, 3. rental car 4. comfortable public transit, 5. economic public transit
+percent_activity_private_car = [0.12, 0.08, 0.8]; %park_at_airport, park_off_airport, curbside 
+percent_activity_rental_car = [0.935, 0.065]; % rentalcar_on_airport, rentalcar_off_airport
+percent_parking_mode = [0.95 0.032 0.018 0]; %short term hourly, short term daily, long term, economic parking 
+%input parameters of parking duration distribution, short term hourly,
+%short tem daily, long term and economic parking in minutes
+%ALL FAKE NUMBERS NOW
 st_hourly_mu = 54/60;
 st_hourly_sigma = 5; %FIXME
 st_daily_mu = 54/60;
@@ -50,12 +45,12 @@ lt_daily_mu = 54/60;
 lt_daily_sigma = 5; %FIXME
 eco_parking_mu = 54/60;
 eco_parking_sigma = 5; %FIXME
-%av_adoption_rate = 0.1; %FIXME
+%av_adoption_rate = [0.1 0.1 0.1 0.1 0.1 0.1]; %FIXME
 
 %------------------create simulation_all table-----------------------------
 
 %contains all information for each ID
-simulation_all = zeros(num_all_IDs, 10);
+simulation_all = zeros(num_all_IDs, 11);
 % 1: IDs, 
 % 2: trip purpose (resident or tourist) 
 % 3: zip codes (for resident only) 
@@ -67,6 +62,9 @@ simulation_all = zeros(num_all_IDs, 10);
 % 8: distance: only for resident
 % 9: AV: whether the vehicle is AV or not, 1 AV, 0 not AV
 % 10: revenue: revenue from this person
+% 11: cost for that person to airport
+% 12: round trip fuel cost
+
 
 %------------------col1 generating IDs-------------------------------------
 simulation_all(:,1) = 1:num_all_IDs;
@@ -117,8 +115,12 @@ num_private_car = sum(simulation_all(:,4)==1);
 distribution_activity_private_car = modes_distribution_by_percentile(num_private_car, percent_activity_private_car);
 j=1;
 for i=1:num_all_IDs
-    if simulation_all(i,4)==1
+    if simulation_all(i,2)==1 && simulation_all(i,4)==1 %resident with private car
         simulation_all(i,5) = distribution_activity_private_car(j);
+        j = j+1;
+    end
+    if simulation_all(i,2)~=1 && simulation_all(i,4)==1
+        simulation_all(i,5) = 3; %assume all tourists who use private car will only use curbised
         j = j+1;
     end
 end
@@ -132,7 +134,7 @@ for i=1:num_all_IDs
     end
 end
 
-%5.3 rental car
+%5.3) rental car
 num_rental_car = sum(simulation_all(:,4)==3);
 distribution_activity_rental_car = modes_distribution_by_percentile(num_rental_car, percent_activity_rental_car);
 j=1;
@@ -143,6 +145,15 @@ for i=1:num_all_IDs
     end
 end
 assert(j-1==length(distribution_activity_rental_car))
+
+%5.4) comfortable GT
+for i=1:num_all_IDs
+    if simulation_all(i,4)==4 %comfortable gt
+        simulation_all(i,5) = 3;
+        j = j+1;
+    end
+end
+
 
 %------------------col6 parking mode---------------------------------------
 %for activity = park_at_ariport only (private car of course )
--- main_resident_tourist.m
+++ main_resident_tourist.m
@@ -7,12 +7,35 @@ clear;
 av_adoption_rates = [0:0.05:1; 0:0.05:1; 0:0.05:1; 0:0.05:1; 0:0.05:1]';%20*5: each row represent 5 adoption rate respectively for: private car, tnc, rental car, comfortable, economic
 num_adoption = size(av_adoption_rates, 1);
 
+%fuel price
+fuel_per_gallon = 2.8;
+MPG = 20;
+mile_in_meter = 1609.34;
+
+%electic vehicle charges on-site, no charges as of 2018
+SFO_charge_fee = 0;
+cost_per_kwh = 0.59; %21 cents per kwh in SF area, but 0.49-0.69 by Blink company, level-2
+kwh_per_mile = 0.2257; %range from 18.75kwh/100mile to 26.39kwh/100mile, thus average 22.57
+full_charge_kwh = 30; %the average EV needs about 30 kWh of electricity to power the vehicle for 100 miles
+time_per_charge = 120; % 2-3 hours http://www.blinknetwork.com/membership-faqs.html
+
+
 for i=1:num_adoption
 %----------------------step1 generate simulation table---------------------
 %1*10, 1: IDs,2: trip purpose 3: zip codes 4: travel modes 5: activity 6: parking mode, 7: parking time 8: distance 9: AV 10: revenue
     simulation_all = generate_simulation_table(av_adoption_rates(i, :));
     num_rows = size(simulation_all);
-
+    
+    cost_resident_fuel = zeros(num_rows,6);%1. ID,  2.cost, 3. round-trip fuel cost  4. choice (0 parking, 1 AV, 2 tnc, 3,comf gt, 4 ), 5. lost parking fee per day, 6 parking cost per day
+    for k =1:num_rows
+    %--------------------------------------------------------------------------%
+    %calculating parking cost
+    cost_resident_fuel(i,1) = simulation_all(i,1); %id
+    cost_resident_fuel(i,2) = simulation_all(i,11); %cost
+    distance_cal = simulation_all(i,8);
+    gallon_per_meter = 1/MPG/mile_in_meter;
+    cost_resident_fuel(i,3) = distance_cal*fuel_per_gallon*gallon_per_meter*2;
+    end
 %----------------------step2 compute revenue for each row------------------
     for j = 1:num_rows
         row = simulation_all(j, :);
@@ -31,8 +54,62 @@ for i=1:num_adoption
         end
         simulation_all(j,10) = revenue;
     end
-
+    
 end
+
+for i=1:num_adoption
+%----------------------step1 generate simulation table---------------------
+%1*10, 1: IDs,2: trip purpose 3: zip codes 4: travel modes 5: activity 6: parking mode, 7: parking time 8: distance 9: AV 10: revenue
+    simulation_all = generate_simulation_table(av_adoption_rates(i, :));
+    num_rows = size(simulation_all);
+
+%----------------------step3 compute cost for each row------------------
+    for j = 1:num_rows
+        row = simulation_all(j, :);
+        cost = 0;
+        if(row(2)==1&&row(4)==1&&row(5)==1) % resident private car
+           cost = revenue_private_car_at_airport(row);
+        end
+        if(row(4)==2) % tnc
+           cost = cost_tnc(row);
+        end
+%         if(row(4)==3) % rental car
+%             revenue = revenue_rental_car(row);
+%         end
+        if row(4)==4 % comfortable 
+           cost = 25;
+        end
+        if row(4)==5 %economic
+            cost = 9.5;
+        end
+        simulation_all(j,11) = cost;
+    end
+    
+end
+
+count_AV_all(n) = count_AV;
+    count_parking_all(n) = sum(count_parking);
+    percent_AV_all(n) = count_AV/(count_AV+sum(count_parking));
+    parking_fee_lost_all(n) = parking_fee_lost;
+    parking_revenue_all(n) = parking_revenue;
+    
+count_AV = 0;
+count_parking = [0;0;0;0];
+for i=1:num_all_IDs
+    if simulation_all(i,2)==1 %resident
+        if simulation_all(i,5)==1 && simulation_all(i,9)==1 && (cost_resident_fuel(i,2)>=cost_resident_fuel(i,3))
+            cost_resident_fuel(i,4) = 1;
+            cost_resident_fuel(i,5) = cost_resident_fuel(i,6); % parking lost
+            count_AV = count_AV + 1;
+        else
+            cost_resident_fuel(i,4) = 0;
+            cost_resident_fuel(i,5) = 0; % no parking lost
+            count_parking(simulation_all(i,5)) = count_parking(simulation_all(i,5)) + 1;
+        end
+    end
+end
+
+
 
 
 
